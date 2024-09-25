c1b37f7fff184ed132bf6a5b708cf839b07f9fef
Dashboard <ayoubahtouch11@gmail.com>
2024-09-25 14:53:30 +0200

    challenge 7

---
+++ Untitled-7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+int a ;
+printf("entre le nombre:");
+scanf("%d",&a);
+if (a%2==0){
+    printf("le nombre est pair ");
+}
+else
+    printf("le nombre est impair");
+    
+return 0;
+}
