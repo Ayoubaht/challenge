447d55ec13888807bf3d90f249dffcfe4f6a21ac
Dashboard <ayoubahtouch11@gmail.com>
2024-09-25 15:39:44 +0200

    challenge 8

---
+++ challenge 2 Untitled-2.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char lettre;
+    printf("entrer lettre:");
+    scanf("%s",&lettre);
+    switch(lettre){
+    case('a'):
+    printf("une voyelle");
+    break;    case('o'):
+    printf("une voyelle");
+    break;    case('i'):
+    printf("une voyelle");
+    break;    case('e'):
+    printf("une voyelle");
+    break;    case('u'):
+    printf("une voyelle");
+    break;
+    default:
+    printf("no une voyelle");
+    }
+    return 0;
+}
